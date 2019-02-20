adba773f2b95725e6a4b03b7ff1f2dd7dd660434
nareshg14 <naresh@globalerp.ca>
2019-02-19 21:57:16 -0500

    Commited by Y person

---
+++ src/test/java/projectMaven/BramptonMVNProject/AddedByYPerson.java
@@ -0,0 +1,10 @@
+package projectMaven.BramptonMVNProject;
+
+public class AddedByYPerson {
+	
+	//New program added by Y person
+	public void newMethod(){
+		System.out.println("This method is added by Y person");
+	}
+
+}
